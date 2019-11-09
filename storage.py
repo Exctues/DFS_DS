7431f92d4f997f40951f35f3d468b69248f09557
exctues <alexnamecode@gmail.com>
2019-11-09 23:43:49 +0300

    constants change

--- storage.py
+++ storage.py
@@ -10,37 +10,38 @@ class NamenodeListener(Thread):
     def __init__(self, sock: socket.socket):
         super().__init__(daemon=True)
         self.sock = sock
-        self.error_code = 0
 
     def run(self):
         # wait for a code
-        code = self.sock.recv(4).decode()
+        code = int(self.sock.recv(4).decode('utf-8'))
         self.sock.send('ok'.encode('utf-8'))
         # dispatch the code
-        if code == 1:  # make_file (not dir)
-            full_path = self.sock.recv(1024).decode()
+        if code == Codes.make_file:
+            full_path = self.sock.recv(1024).decode('utf-8')
             CommandHandler.handle_make_file(full_path)
-        elif code == 4:  # rm
-            full_path = self.sock.recv(1024).decode()
-            CommandHandler.handle_rmdir(full_path)
-        elif code == 6:  # copy
-            source = self.sock.recv(1024).decode()
+        elif code == Codes.rm:
+            full_path = self.sock.recv(1024).decode('utf-8')
+            CommandHandler.handle_rm(full_path)
+        elif code == Codes.copy:
+            source = self.sock.recv(1024).decode('utf-8')
             self.sock.send('ok'.encode('utf-8'))
-            destination = self.sock.recv(1024).decode()
+            destination = self.sock.recv(1024).decode('utf-8')
             CommandHandler.handle_copy(source, destination)
-        elif code == 7:  # move
-            source = self.sock.recv(1024).decode()
+        elif code == Codes.move:
+            source = self.sock.recv(1024).decode('utf-8')
             self.sock.send('ok'.encode('utf-8'))
-            destination = self.sock.recv(1024).decode()
+            destination = self.sock.recv(1024).decode('utf-8')
             CommandHandler.handle_move(source, destination)
-        elif code == 9:  # mkdir
-            full_path = self.sock.recv(1024).decode()
+        elif code == Codes.make_dir:
+            full_path = self.sock.recv(1024).decode('utf-8')
             CommandHandler.handle_mkdir(full_path)
+        elif code == Codes.rmdir:
+            full_path = self.sock.recv(1024).decode('utf-8')
+            CommandHandler.handle_rmdir(full_path)
         else:
-            print("Namenode: no command correspond to code", code)
-            error_code = -1
+            print("NamenodeListener: no command correspond to code", code)
 
-        # self.sock.send(str(self.error_code).encode('utf-8'))
+        # TODO if doesn't work change to shutdown()
         self.sock.close()
 
 
@@ -48,30 +49,41 @@ class ClientListener(Thread):
     def __init__(self, sock: socket.socket):
         super().__init__(daemon=True)
         self.sock = sock
-        self.error_code = 0
 
     def _close(self):
         self.sock.close()
 
     def run(self):
-        code = self.sock.recv(4).decode()
+        code = self.sock.recv(4).decode('utf-8')
         self.sock.send('ok'.encode('utf-8'))
         # dispatch the code
-        if code == 2:  # print
-            full_path = self.sock.recv(1024).decode()
-            CommandHandler.handle_print(full_path)
-        elif code == 3:  # upload
-            full_path = self.sock.recv(1024).decode()
-            CommandHandler.handle_print(full_path)
+        if code == Codes.print:
+            full_path = self.sock.recv(1024).decode('utf-8')
+            CommandHandler.handle_print(self.sock, full_path)
+        elif code == Codes.upload:
+            full_path = self.sock.recv(1024).decode('utf-8')
+            CommandHandler.handle_print(self.sock, full_path)
+        else:
+            print("ClientListener: no command correspond to code", code)
 
-        # self.sock.send(str(self.error_code).encode('utf-8'))
+        # TODO if doesn't work change to shutdown()
         self.sock.close()
 
 
-def init_sync():
+def get_sync_storage_ip():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((Constants.NAMENODE_IP, Constants.NEW_NODES_PORT))
-    to_sync = sock.recv(1024).decode()
+    to_sync = sock.recv(1024).decode('utf-8')
+    sock.close()
+    return to_sync
+
+
+def init_sync():
+    storage_ip = get_sync_storage_ip()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((storage_ip, Constants.STORAGE_PORT))
+    # TODO: gather all files from another storage
+    # TODO: Also notify namenode that this node is clear now
 
 
 def main():
