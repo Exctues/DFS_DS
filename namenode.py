384063dc148eb772c28cf7582ee38b1cc827262e
whst13377 <whst13377@gmail.com>
2019-11-09 22:46:14 +0300

    cha

--- namenode.py
+++ namenode.py
@@ -1,9 +1,11 @@
 # N cmd data
 import os
+import random
 import time
 import socket
 import sys
 from namenode.handlers import Handlers
+from namenode.tree import FSTree
 from constants import Constants
 
 from threading import Thread, Lock
@@ -42,12 +44,26 @@ def new_nodes_listener():
             with storage_nodes.lock:
                 storage_nodes.nodes.add(addr)
 
+            con.send(random.sample(storage_nodes, 1).encode('utf-8'))
+            # ack
+            # a = con.recv(1024).decode('utf-8')
+
             con.close()
 
     new_nodes_listener = Thread(target=new_nodes_listener_thread)
     new_nodes_listener.start()
 
+tree = FSTree()
+
+while True:
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    soc.bind(('', Constants.CLIENT_TO_NAMENODE))
+    soc.listen()
+    while True:
+        con, addr = soc.accept()
+
+        cmd = con.recv(1024).decode('utf-8')
 
 
-#filesystem = Tree()
-# Heartbeat().start()
+        con.close()
--- namenode/handlers.py
+++ namenode/handlers.py
@@ -3,23 +3,37 @@ import socket
 from threading import Thread
 from codes import Codes
 
+
 class Handlers:
 
     @staticmethod
-    def send_args(ip, port, command):
+    def send_args(ip, port, cmd, arg1='', arg2=''):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((ip, port))
-        sock.send(command.encode('utf-8'))
+
+        sock.send(cmd.encode('utf-8'))
+        # acknowledge
+        a = sock.recv(1024).decode('utf-8')
+        sock.send(arg1.encode('utf-8'))
+        if arg2 != '':
+            # acknowledge
+            a = sock.recv(1024).decode('utf-8')
+            sock.send(arg2.encode('utf-8'))
+
         sock.close()
 
     @staticmethod
-    def multicast(ips, port, command):
+    def multicast(ips, port, cmd, arg1='', arg2=''):
         for ip in ips:
-            thread = Thread(target=Handlers.send_args, args=[ip, port, command])
+            thread = Thread(target=Handlers.send_args, args=[ip, port, cmd, arg1, arg2])
             thread.start()
 
     @staticmethod
     def make_file():
+        pass
+
+    @staticmethod
+    def init():
         pass
 
     @staticmethod
@@ -51,17 +65,20 @@ class Handlers:
 
     @staticmethod
     def ls():
-        #TODO: куда и кого послать
+        # TODO: куда и кого послать
         Handlers.send_args()
 
     @staticmethod
     def mkdir(ips, port, tree, filepath):
-        Handlers.multicast(ips, port, 'N'+getattr(Codes,'make_dir')+filepath)
+        Handlers.multicast(ips, port, getattr(Codes, 'make_dir'), filepath)
         # TODO: add filesize to tree.insert and to send
         tree.insert(filepath)
 
     @staticmethod
     def rmdir(ips, port, tree, filepath):
-        Handlers.multicast(ips, port, 'N' + getattr(Codes, 'rmdir') + filepath)
+        Handlers.multicast(ips, port, getattr(Codes, 'rmdir'), filepath)
         # TODO: add filesize to tree.insert and to send
         tree.insert(filepath)
+
+
+# proveryat' chto source suchshestvuet v dereve
