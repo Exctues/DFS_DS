a83e07df6d5da8021349d37b680a4924ded9640f
whst13377 <whst13377@gmail.com>
2019-11-09 15:41:52 +0300

    added codes.py

---
+++ codes.py
@@ -0,0 +1,18 @@
+class Codes:
+    help = -3
+    cd = -2
+    pwd = -1
+
+    init = 0
+
+    make_file = 1
+    print = 2
+    upload = 3
+    rm = 4
+    info = 5
+    copy = 6
+    move = 7
+
+    ls = 8
+    make_dir = 9
+    rmdir = 10
