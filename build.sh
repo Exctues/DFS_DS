c480e7c0cbdd2c4d4aba494c3e49654baef294fa
whst13377 <whst13377@gmail.com>
2019-11-17 05:08:33 +0300

    fixed dockerfile for client

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
+sudo cp logger.py client_docker/logger.py
+sudo cp codes.py client_docker/codes.py
+sudo cp parameters.py client_docker/parameters.py
+
+sudo cp logger.py namenode_docker/logger.py
+sudo cp codes.py namenode_docker/codes.py
+sudo cp parameters.py namenode_docker/parameters.py
+
+sudo cp logger.py storage_docker/logger.py
+sudo cp codes.py storage_docker/codes.py
+sudo cp parameters.py storage_docker/parameters.py
+
+sudo cp constants.py client_docker/constants.py
+sudo cp constants.py namenode_docker/constants.py
+sudo cp constants.py storage_docker/constants.py
+
 
 sudo docker build -t=client:2.0 client_docker
 sudo docker tag client:2.0 concrete13377/client:2.0
@@ -11,9 +27,6 @@ sudo docker build -t=storage:2.0 storage_docker
 sudo docker tag storage:2.0 concrete13377/storage:2.0
 sudo docker push concrete13377/storage:2.0
 
-# to deploy: doesnt really works yet, use run_locally.sh
-# docker swarm init
-# docker stack deploy -c docker-compose.yml mydfs
 
-# sudo docker stack rm mydfs
-# sudo docker service ps mydfs_client
+# now how to actually use it
+#
