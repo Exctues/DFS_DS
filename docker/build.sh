3190596d7d7024352a13de3dbf8884c7d7385f6f
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2019-11-17 23:26:52 +0300

    better structure skeleton

--- docker/build.sh
+++ docker/build.sh
@@ -14,18 +14,34 @@ sudo cp constants.py client_docker/constants.py
 sudo cp constants.py namenode_docker/constants.py
 sudo cp constants.py storage_docker/constants.py
 
+docker build -t=client:3.0 client_docker
+docker tag client:3.0 concrete13377/client:3.0
+docker push concrete13377/client:3.0
 
-sudo docker build -t=client:3.0 client_docker
-sudo docker tag client:3.0 concrete13377/client:3.0
-sudo docker push concrete13377/client:3.0
+docker build -t=namenode:3.0 namenode_docker
+docker tag namenode:3.0 concrete13377/namenode:3.0
+docker push concrete13377/namenode:3.0
 
-sudo docker build -t=namenode:3.0 namenode_docker
-sudo docker tag namenode:3.0 concrete13377/namenode:3.0
-sudo docker push concrete13377/namenode:3.0
+docker build -t=storage:3.0 storage_docker
+docker tag storage:3.0 concrete13377/storage:3.0
+docker push concrete13377/storage:3.0
 
-sudo docker build -t=storage:3.0 storage_docker
-sudo docker tag storage:3.0 concrete13377/storage:3.0
-sudo docker push concrete13377/storage:3.0
+# clean-up to avoid code duplication
+rm client_docker/logger.py
+rm client_docker/codes.py
+rm client_docker/parameters.py
+
+rm namenode_docker/logger.py
+rm namenode_docker/codes.py
+rm namenode_docker/parameters.py
+
+rm storage_docker/logger.py
+rm storage_docker/codes.py
+rm storage_docker/parameters.py
+
+rm client_docker/constants.py
+rm namenode_docker/constants.py
+rm storage_docker/constants.py
 
 
 ## now how to actually use it
