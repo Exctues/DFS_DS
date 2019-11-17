41f0b3a7cb4c7f28f9f049ebeb31dd1541ddfc6a
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2019-11-18 02:22:07 +0300

    dummy commit to push to remote

--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ sudo cp constants.py client_docker/constants.py
 sudo cp constants.py namenode_docker/constants.py
 sudo cp constants.py storage_docker/constants.py
 
-
 sudo docker build -t=client:3.0 client_docker
 sudo docker tag client:3.0 concrete13377/client:3.0
 sudo docker push concrete13377/client:3.0
@@ -26,7 +25,6 @@ sudo docker push concrete13377/namenode:3.0
 sudo docker build -t=storage:3.0 storage_docker
 sudo docker tag storage:3.0 concrete13377/storage:3.0
 sudo docker push concrete13377/storage:3.0
-
 
 ## now how to actually use it
 ## чтобы подключиться в сварм на воркинг ноде нужно к той команде которую swarm manager дает
