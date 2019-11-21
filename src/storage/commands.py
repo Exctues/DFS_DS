3ebaccbf92bce07fa2b6c1a8825e7bcf49fc7878
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2019-11-21 22:36:36 +0300

    better logging for dir creation

--- src/storage/commands.py
+++ src/storage/commands.py
@@ -127,7 +127,8 @@ class CommandHandler:
     def handle_mkdir(full_path: str):
         # to properly join
         full_path = full_path.strip(os.sep)
-        os.makedirs(os.path.join(Constants.STORAGE_PATH, full_path), exist_ok=True)
+        full_path = os.path.join(Constants.STORAGE_PATH, full_path)
+        os.makedirs(full_path, exist_ok=True)
         if os.path.exists(full_path):
             return True
 
